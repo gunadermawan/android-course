6347a52bc75cdb12d3479b4d8039f91df577fd9e
gunadermawan <gunadermawan.official@gmail.com>
2024-07-21 02:04:01 +0700

    course: viewGroup

---
+++ app/src/main/java/com/gun/course/viewGroup/ViewgroupActivity.kt
@@ -0,0 +1,21 @@
+package com.gun.course.viewGroup
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.gun.course.R
+
+class ViewgroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_viewgroup)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
