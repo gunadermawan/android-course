2dcff50381ff70b02c1ed480d6d8dbbd448ce79f
gunadermawan <gunadermawan.official@gmail.com>
2024-07-24 00:22:14 +0700

    course: sideEffect

--- app/src/main/java/com/gun/course/ui/sideEffect/SideEffect.kt
+++ app/src/main/java/com/gun/course/ui/sideEffect/SideEffect.kt
@@ -1,6 +1,9 @@
 package com.gun.course.ui.sideEffect
 
 import android.location.LocationListener
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,16 +15,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gun.course.ui.theme.CourseAppTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 // launchedEffect
+
 @Composable
 fun DataFetcher(modifier: Modifier = Modifier) {
-    var data by remember { mutableStateOf("Loading data") } // init state
+    var data by remember {
+        mutableStateOf("Loading")
+    } // state
     LaunchedEffect(Unit) {
         data = fetchDataFromNetwork()
     }
@@ -29,44 +35,45 @@ fun DataFetcher(modifier: Modifier = Modifier) {
 }
 
 
-// disposableEffect
 @Composable
-fun LocationTracking(modifier: Modifier = Modifier) {
-    DisposableEffect(Unit) {
-        val locationListener = LocationListener {
-            // handle locationlistener
-        }
-//        startTracking(locationListener)
-        onDispose {
-//            stopTracking(locationListener)
-        }
-    }
+fun LocationTracker(modifier: Modifier = Modifier) {
+//    DisposableEffect(Unit) {
+//        val locationListener = LocationListener {
+//            //
+//        }
+////        startTrackingLocation(locationListener)
+////        //end of recompose
+////        onDispose {
+////            stopTrackingLocation(locationListener)
+////        }
+//    }
 }
 
-// rememberCoroutineScope
+
+// rememberScope
+
 @Composable
 fun FetchButton(modifier: Modifier = Modifier) {
     val scope = rememberCoroutineScope()
-    Button(onClick = {
-        scope.launch {
-            val data = fetchDataFromNetwork()
-            return@launch
+    var data by remember { mutableStateOf("Initial Text") }
+    Column {
+        Button(onClick = { scope.launch { data = fetchDataFromNetwork() } }) {
+            Text(text = "Fetch Data")
         }
-    }) {
-        Text(text = "Fetch data")
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = data)
     }
 }
 
 suspend fun fetchDataFromNetwork(): String {
     delay(2000)
-    return "Hello, compose here!"
+    return "Hello, compose here"
 }
 
 @Preview
 @Composable
-private fun DataFetcherPreview() {
+private fun PreviewSideEffect() {
     CourseAppTheme {
-//        DataFetcher()
         FetchButton()
     }
 }
