63f7138267fb0e1d7e9225b7cfeba5958ffb9124
gunadermawan <gunadermawan.official@gmail.com>
2024-07-25 15:11:01 +0700

    course: basic layouts

---
+++ app/src/main/java/com/gun/course/ui/basicLayouts/BasicLayouts.kt
@@ -0,0 +1,59 @@
+package com.gun.course.ui.basicLayouts
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gun.course.R
+import com.gun.course.ui.theme.CourseAppTheme
+
+@Composable
+fun ContactCard(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.padding(8.dp)) {
+        Box {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+            )
+            Icon(
+                imageVector = Icons.Default.CheckCircle,
+                contentDescription = null,
+                tint = Color.Green,
+                modifier = Modifier.align(Alignment.BottomEnd)
+            )
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = "Android", fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ContactCardPreview() {
+    CourseAppTheme {
+        ContactCard()
+    }
+}
