8c567926f03419b6a0dd51a2a0c615240b85271b
gunadermawan <gunadermawan.official@gmail.com>
2024-07-31 17:03:58 +0700

    course: basic layuts update

--- app/src/main/java/com/gun/course/ui/basicLayouts/BasicLayouts.kt
+++ app/src/main/java/com/gun/course/ui/basicLayouts/BasicLayouts.kt
@@ -4,11 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Icon
@@ -16,44 +12,52 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gun.course.R
 import com.gun.course.ui.theme.CourseAppTheme
 
 @Composable
-fun ContactCard(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.padding(8.dp)) {
-        Box {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(60.dp)
-                    .clip(CircleShape)
-            )
-            Icon(
-                imageVector = Icons.Default.CheckCircle,
-                contentDescription = null,
-                tint = Color.Green,
-                modifier = Modifier.align(Alignment.BottomEnd)
-            )
-        }
-        Spacer(modifier = Modifier.width(8.dp))
+fun ArtistColumns(modifier: Modifier = Modifier) {
+    Row {
+        Text(text = "Art Siloam")
+        Text(text = "5 Minutes ago ")
+    }
+}
+
+@Composable
+fun ArtistCardRow(modifier: Modifier = Modifier) {
+    Row {
+        Image(
+            modifier = Modifier.size(50.dp),
+            painter = painterResource(id = R.drawable.img_1),
+            contentDescription = null
+        )
         Column {
-            Text(text = "Android", fontWeight = FontWeight.Bold)
+            Text(text = "Art Siloam")
+            Text(text = "5 Minutes ago ")
         }
     }
 }
+
+@Composable
+fun ArtistAvatar(modifier: Modifier = Modifier) {
+    Box(contentAlignment = Alignment.BottomEnd) {
+        Image(
+            modifier = Modifier.size(50.dp),
+            painter = painterResource(id = R.drawable.img_1),
+            contentDescription = null
+        )
+        Icon(Icons.Default.CheckCircle, contentDescription = null, modifier = Modifier.size(20.dp))
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 private fun ContactCardPreview() {
     CourseAppTheme {
-        ContactCard()
+        ArtistAvatar()
     }
 }
