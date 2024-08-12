588ae989b2239109e073b2c1491d84b0fbf99752
gunadermawan <gunadermawan.official@gmail.com>
2024-08-12 14:30:10 +0700

    course: datastore

--- app/src/main/java/com/gun/course/ComposeActivity.kt
+++ app/src/main/java/com/gun/course/ComposeActivity.kt
@@ -1,7 +1,6 @@
 package com.gun.course
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,16 +9,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -28,15 +24,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.gun.course.model.Post
-import com.gun.course.network.RetrofitInstance
-import com.gun.course.repository.PostRepo
 import com.gun.course.ui.theme.CourseAppTheme
+import com.gun.course.utils.IS_LOGIN_KEY
+import com.gun.course.utils.USERNAME_KEY
+import com.gun.course.utils.dataStore
 import com.gun.course.utils.getSharedPreference
 import com.gun.course.utils.savePreference
-import com.gun.course.viewmodel.PostViewModel
-import com.gun.course.viewmodel.PostViewModelFactory
+import com.gun.course.utils.saveToDataStore
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class ComposeActivity : ComponentActivity() {
@@ -46,69 +41,45 @@ class ComposeActivity : ComponentActivity() {
         setContent {
             CourseAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SharedPrefScreen(modifier = Modifier.padding(innerPadding))
+                    DataStoreScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun NetworkRequest(modifier: Modifier = Modifier) {
-    var posts by remember {
-        mutableStateOf<List<Post>>(emptyList())
+fun DataStoreScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    var username by remember {
+        mutableStateOf("")
     }
-    val scope = rememberCoroutineScope()
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp)
-    ) {
-        LazyColumn(modifier = Modifier.weight(1f)) {
-            items(posts) { post ->
-                Text(text = post.title, modifier = Modifier.padding(bottom = 16.dp))
-            }
-        }
-        Button(onClick = {
-            scope.launch {
-                try {
-                    val post = RetrofitInstance.api.getPost()
-                    posts = post.take(10)
-                    Log.d("TAG DEBOS", "NetworkRequest: enter try")
-                } catch (e: Exception) {
-                    Log.e("TAG ERROR", "NetworkRequest: ${e.message}")
-                }
-            }
-        }) {
-            Text(text = "get data")
-        }
+    val isLoggedIn by remember {
+        mutableStateOf(false)
     }
-}
 
-@Composable
-fun PostScreen(
-    modifier: Modifier = Modifier,
-    postViewModel: PostViewModel = viewModel(
-        factory = PostViewModelFactory(
-            PostRepo(RetrofitInstance.api)
+    val userPreferences by context.dataStore.data
+        .map {
+            Pair(
+                it[USERNAME_KEY] ?: "",
+                it[IS_LOGIN_KEY] ?: false
+            )
+        }.collectAsState(initial = Pair("", false))
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(text = "userName: ${userPreferences.first}")
+        Text(text = "isLoggedIn: ${userPreferences.second}")
+        Spacer(modifier = modifier.height(8.dp))
+        OutlinedTextField(
+            value = username,
+            onValueChange = {
+                username = it
+            },
         )
-    )
-) {
-    val posts by postViewModel.posts.observeAsState(emptyList())
-    LazyColumn {
-        items(posts.size) { index ->
-            val post = posts[index]
-            PostItem(post = post)
+        Spacer(modifier = modifier.height(8.dp))
+        Button(onClick = { coroutineScope.launch { saveToDataStore(context, username, true) } }) {
+            Text(text = "Save to Datastore")
         }
-    }
-}
-
-@Composable
-fun PostItem(post: Post) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = post.title, style = MaterialTheme.typography.titleMedium)
-        Text(text = post.body, style = MaterialTheme.typography.bodyMedium)
     }
 }
 
@@ -144,6 +115,6 @@ fun SharedPrefScreen(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CourseAppTheme {
-        SharedPrefScreen()
+        DataStoreScreen()
     }
 }
--- app/src/main/java/com/gun/course/utils/SharedPref.kt
+++ app/src/main/java/com/gun/course/utils/SharedPref.kt
@@ -1,19 +1,51 @@
 package com.gun.course.utils
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-    fun savePreference(context: Context, userName: String, isLogin: Boolean) {
-        val sharedPref = context.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
-        with(sharedPref.edit()) {
-            putString("username", userName)
-            putBoolean("isLoggedIn", isLogin)
-            apply()
+
+//Define Keys
+
+val USERNAME_KEY = stringPreferencesKey("username")
+val IS_LOGIN_KEY = booleanPreferencesKey("isLoggedIn")
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+suspend fun saveToDataStore(context: Context, username: String, isLogin: Boolean) {
+    context.dataStore.edit {
+        it[USERNAME_KEY] = username
+        it[IS_LOGIN_KEY] = isLogin
+    }
+}
+
+fun getUserPreferencesFlow(context: Context): Flow<Pair<String, Boolean>> {
+    return context.dataStore.data
+        .map {
+            val username = it[USERNAME_KEY] ?: ""
+            val isLogin = it[IS_LOGIN_KEY] ?: false
+            Pair(username, isLogin)
         }
-    }
+}
 
-    fun getSharedPreference(context: Context): Pair<String, Boolean> {
-        val sharedPref = context.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
-        val userName = sharedPref.getString("username", "") ?: ""
-        val isLoggedIn = sharedPref.getBoolean("isLoggedIn", false)
-        return Pair(userName, isLoggedIn)
+fun savePreference(context: Context, userName: String, isLogin: Boolean) {
+    val sharedPref = context.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
+    with(sharedPref.edit()) {
+        putString("username", userName)
+        putBoolean("isLoggedIn", isLogin)
+        apply()
     }
+}
+
+fun getSharedPreference(context: Context): Pair<String, Boolean> {
+    val sharedPref = context.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
+    val userName = sharedPref.getString("username", "") ?: ""
+    val isLoggedIn = sharedPref.getBoolean("isLoggedIn", false)
+    return Pair(userName, isLoggedIn)
+}
