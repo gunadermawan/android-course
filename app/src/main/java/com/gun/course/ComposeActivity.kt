f340b27cbaecb21e3532032c96a97bbd09383fef
gunadermawan <gunadermawan.official@gmail.com>
2024-08-31 13:53:34 +0700

    course: vector animation

--- app/src/main/java/com/gun/course/ComposeActivity.kt
+++ app/src/main/java/com/gun/course/ComposeActivity.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +24,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.gun.course.ui.theme.CourseAppTheme
 
@@ -38,13 +50,27 @@ class ComposeActivity : ComponentActivity() {
         setContent {
             CourseAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ValueAnimationSample(modifier = Modifier.padding(innerPadding))
+                    HearAnimation(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+
+@Composable
+fun HearAnimation(modifier: Modifier = Modifier) {
+    val color by animateColorAsState(
+        targetValue = if (isSystemInDarkTheme()) Color.Red else Color.Black,
+        animationSpec = tween(durationMillis = 5000)
+    )
+    Icon(
+        imageVector = Icons.Filled.Favorite,
+        contentDescription = "Heart",
+        tint = color,
+        modifier = modifier.size(100.dp)
+    )
+}
 
 @Composable
 fun ValueAnimationSample(modifier: Modifier = Modifier) {
