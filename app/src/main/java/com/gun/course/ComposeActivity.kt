21b973febd3a0d10bd417ab69eb6e6b8a62517ac
gunadermawan <gunadermawan.official@gmail.com>
2024-09-10 09:01:09 +0700

    course: intent camera and gallery

--- app/src/main/java/com/gun/course/ComposeActivity.kt
+++ app/src/main/java/com/gun/course/ComposeActivity.kt
@@ -1,11 +1,21 @@
 package com.gun.course
 
-import android.media.AudioAttributes
-import android.media.SoundPool
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,25 +23,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 import com.gun.course.ui.theme.CourseAppTheme
 
 class ComposeActivity : ComponentActivity() {
@@ -41,13 +54,14 @@ class ComposeActivity : ComponentActivity() {
         setContent {
             CourseAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ExoPlayerExample(
+                    CameraAndGalleryExample(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
+
 
     @Composable
     fun ExoPlayerExample(modifier: Modifier = Modifier) {
@@ -74,13 +88,15 @@ class ComposeActivity : ComponentActivity() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            AndroidView(factory = { context ->
-                PlayerView(context).apply {
-                    player = exoPlayer
-                }
-            }, modifier = modifier
-                .fillMaxWidth()
-                .height(300.dp))
+            AndroidView(
+                factory = { context ->
+                    PlayerView(context).apply {
+                        player = exoPlayer
+                    }
+                }, modifier = modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+            )
             Spacer(modifier = modifier.height(16.dp))
             Button(onClick = {
                 if (exoPlayer.isPlaying) {
@@ -95,45 +111,77 @@ class ComposeActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalPermissionsApi::class)
     @Composable
-    fun SoundPoolExample(modifier: Modifier = Modifier) {
+    fun CameraAndGalleryExample(modifier: Modifier = Modifier) {
+        val cameraPermissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
         val context = LocalContext.current
-        var soundPool by remember {
-            mutableStateOf<SoundPool?>(null)
+        var imageUri by remember {
+            mutableStateOf<Uri?>(null)
         }
-        var soundId by remember { mutableIntStateOf(0) }
 
-        LaunchedEffect(key1 = Unit) {
-            val audioAttributes = AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_GAME)
-                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                .build()
-
-            soundPool = SoundPool.Builder()
-                .setMaxStreams(1)
-                .setAudioAttributes(audioAttributes)
-                .build()
-
-            soundId = soundPool!!.load(context, R.raw.clinking_glasses, 1)
-        }
-        DisposableEffect(key1 = Unit) {
-            onDispose {
-                soundPool?.release()
-                soundPool = null
+        val cameraLauncher =
+            rememberLauncherForActivityResult(contract = ActivityResultContracts.TakePicturePreview()) {
+                it?.let {
+                    val uri = saveImageToInternalStorage(context, it)
+                    imageUri = uri
+                }
             }
-        }
+
+        val galleryLauncher =
+            rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data
+                    imageUri = data?.data
+                }
+            }
+
         Column(
             modifier = modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(onClick = {
-                soundPool?.play(soundId, 1f, 1f, 1, 0, 1f)
-            }) {
-                Text(text = "Mainkan suara klik")
+            if (cameraPermissionState.status.isGranted) {
+                Button(onClick = { cameraLauncher.launch(null) }) {
+                    Text(text = "Take picture")
+                }
+            } else {
+                Text(text = "Camera permission is required")
+                Spacer(modifier = modifier.height(16.dp))
+                Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                    Text(text = "request camera permission")
+                }
             }
+            Spacer(modifier = modifier.height(16.dp))
 
+            Button(onClick = {
+                val intent = Intent(Intent.ACTION_PICK).apply { type = "image/*" }
+                galleryLauncher.launch(intent)
+            }) {
+                Text(text = "pick image from gallery")
+            }
+            Spacer(modifier = modifier.height(16.dp))
+            imageUri?.let {
+                val bitmap = if (Build.VERSION.SDK_INT < 28) {
+                    MediaStore.Images.Media.getBitmap(context.contentResolver, it)
+                } else {
+                    val source = ImageDecoder.createSource(context.contentResolver, it)
+                    ImageDecoder.decodeBitmap(source)
+                }
+                Image(
+                    bitmap = bitmap.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = modifier.size(200.dp)
+                )
+            }
         }
     }
 
+    fun saveImageToInternalStorage(context: Context, bitmap: Bitmap): Uri? {
+        val filename = "photo_${System.currentTimeMillis()}.jpg"
+        val outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        outputStream.close()
+        return Uri.fromFile(context.getFileStreamPath(filename))
+    }
 }
